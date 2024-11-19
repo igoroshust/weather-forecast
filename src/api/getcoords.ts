0b2f6cbe73420da862e791e031a4071a106355d8
Igor <igoroshust75@gmail.com>
2024-11-19 22:35:18 +0900

    fix: hide api key

--- src/api/getcoords.ts
+++ src/api/getcoords.ts
@@ -3,7 +3,6 @@
 import { $geocoderApi, $weatherApi } from "./axios";
 import CurrentCity from "../components/CurrentCity";
 
-// const API_KEY = "3f1bc5548adac751f33f188b7776bcbf";
 
 const getCoordsByCity = (cityName) => {
     return $geocoderApi.get(`direct?q=${cityName}&limit=1&appid=${process.env.REACT_APP_API_KEY}`)
